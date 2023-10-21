0f62e18f4599f225c1f0e0556878a2370f5b2f7a
Judah Goldstein <yehudah.lev@gmail.com>
2023-10-21 00:01:52 -0400

    fin

--- Implimentation/Nutrifit/src/deliverable1tests/MealGraphExample.java
+++ Implimentation/Nutrifit/src/deliverable1tests/MealGraphExample.java
@@ -1,41 +1,41 @@
 package deliverable1tests;
 
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.CategoryPlot;
-import org.jfree.data.category.DefaultCategoryDataset;
-import javax.swing.*;
+//import org.jfree.chart.ChartFactory;
+//import org.jfree.chart.ChartPanel;
+//import org.jfree.chart.JFreeChart;
+//import org.jfree.chart.plot.PlotOrientation;
+//import org.jfree.chart.plot.CategoryPlot;
+//import org.jfree.data.category.DefaultCategoryDataset;
+//import javax.swing.*;
 
 public class MealGraphExample {
-    public static void main(String[] args) {
-        // Create a dataset
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.addValue(3*25, "Meals", "Breakfast");
-        dataset.addValue(5*25, "Meals", "Lunch");
-        dataset.addValue(2*25, "Meals", "Dinner");
-
-        // Create a bar chart
-        JFreeChart barChart = ChartFactory.createBarChart(
-                "Meal Consumption", // Chart title
-                "Meal Type",        // Category axis label
-                "CaloriesCount",            // Value axis label
-                dataset,            // Dataset
-                PlotOrientation.VERTICAL,
-                true, true, false
-        );
-
-        // Create a ChartPanel to display the chart in a Swing application
-        ChartPanel chartPanel = new ChartPanel(barChart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
-
-        // Create a JFrame to hold the chart
-        JFrame frame = new JFrame("Meal Bar Chart");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(chartPanel);
-
-        frame.pack();
-        frame.setVisible(true);
-    }
+//    public static void main(String[] args) {
+//        // Create a dataset
+//        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+//        dataset.addValue(3*25, "Meals", "Breakfast");
+//        dataset.addValue(5*25, "Meals", "Lunch");
+//        dataset.addValue(2*25, "Meals", "Dinner");
+//
+//        // Create a bar chart
+//        JFreeChart barChart = ChartFactory.createBarChart(
+//                "Meal Consumption", // Chart title
+//                "Meal Type",        // Category axis label
+//                "CaloriesCount",            // Value axis label
+//                dataset,            // Dataset
+//                PlotOrientation.VERTICAL,
+//                true, true, false
+//        );
+//
+//        // Create a ChartPanel to display the chart in a Swing application
+//        ChartPanel chartPanel = new ChartPanel(barChart);
+//        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
+//
+//        // Create a JFrame to hold the chart
+//        JFrame frame = new JFrame("Meal Bar Chart");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.getContentPane().add(chartPanel);
+//
+//        frame.pack();
+//        frame.setVisible(true);
+//    }
 }
