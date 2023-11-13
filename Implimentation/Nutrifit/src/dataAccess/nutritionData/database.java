52b438c984cc7b58a531b8ca5985a3611e9b0a5d
Judah Goldstein <yehudah.lev@gmail.com>
2023-11-12 21:09:30 -0500

    cont.

--- Implimentation/Nutrifit/src/dataAccess/nutritionData/database.java
+++ Implimentation/Nutrifit/src/dataAccess/nutritionData/database.java
@@ -1,5 +1,23 @@
 package dataAccess.nutritionData;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class database {
+    public static void main(String[] args) {
+        createDatabase();
+    }
 
+    private static void createDatabase() {
+        String url = "jdbc:sqlite:src/dataAccess/nutritionData/nutrition.db";
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("Nutrition database created." + meta.getDriverName());
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
