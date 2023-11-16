6c14ecc6e1a5eba5310d00fbfa81eb3844f79a42
Long Lin <lonlin@my.yorku.ca>
2023-11-16 13:55:40 -0500

    Create NutrientIntakeChart
    NutrientIntakeChart - pie chart, move mouse over pie chart to see percentages

--- Implimentation/Nutrifit/src/MainUI/UserMainUIScreen.java
+++ Implimentation/Nutrifit/src/MainUI/UserMainUIScreen.java
@@ -2,12 +2,15 @@ package MainUI;
 
 import application.userManager;
 import dataAccess.userData.profile;
+import org.jfree.chart.ChartPanel;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserMainUIScreen {
     private profile user;
@@ -28,6 +31,7 @@ public class UserMainUIScreen {
         JButton logExerciseButton = new JButton("Log Exercise");
 //        JButton mealSummaryButton = new JButton("View Meal Summary");
         JButton CalorieExerciseChartButton = new JButton("Calorie&Exercise Chart");
+        JButton nutrientIntakeChartButton = new JButton("Nutrient Intake Chart");
         JButton editProfileButton = new JButton("Edit Profile");
         JButton deleteProfileButton = new JButton("Delete Profile");
         JButton settingsButton = new JButton("Settings");
@@ -39,6 +43,7 @@ public class UserMainUIScreen {
         mainPanel.add(logExerciseButton);
 //        mainPanel.add(mealSummaryButton);
         mainPanel.add(CalorieExerciseChartButton);
+        mainPanel.add(nutrientIntakeChartButton);
         mainPanel.add(editProfileButton);
         mainPanel.add(deleteProfileButton);
         mainPanel.add(settingsButton);
@@ -76,6 +81,13 @@ public class UserMainUIScreen {
             @Override
             public void actionPerformed(ActionEvent e) {
                 showCalorieExerciseChart(user);
+            }
+        });
+
+        nutrientIntakeChartButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showNutrientIntakeChart();
             }
         });
 
@@ -149,7 +161,7 @@ public class UserMainUIScreen {
     private void showCalorieExerciseChart(profile user) {
         JFrame calorieExerciseChartFrame = new JFrame("CalorieExerciseChart Management");
         calorieExerciseChartFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        calorieExerciseChartFrame.setSize(1000, 900);
+        calorieExerciseChartFrame.setSize(1000, 800);
 
         CalorieExerciseChart calorieExerciseChart = new CalorieExerciseChart("Calorie and Exercise Chart", user);
 
@@ -158,6 +170,52 @@ public class UserMainUIScreen {
         calorieExerciseChartFrame.setVisible(true);
     }
 
+    private void showNutrientIntakeChart() {
+        // Create and display the NutrientIntakeChart
+        JFrame nutrientIntakeChartFrame = new JFrame("Nutrient Intake Chart");
+        nutrientIntakeChartFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        nutrientIntakeChartFrame.setSize(1000, 800);
+
+        // Create an instance of NutrientIntakeChart
+        NutrientIntakeChart nutrientIntakeChart = new NutrientIntakeChart("Nutrient Intake Chart");
+
+        // Set dummy data for testing (replace with actual data retrieval logic)
+        Map<String, Double> dummyData = new HashMap<>();
+        dummyData.put("Proteins", 60.0);
+        dummyData.put("Carbs", 130.0);
+        dummyData.put("Fats", 25.0);
+
+        // Update the nutrient data in the chart
+        nutrientIntakeChart.updateNutrientData(dummyData);
+
+        // Add the NutrientIntakeChart to the NutrientIntakeChartFrame
+        nutrientIntakeChartFrame.add(nutrientIntakeChart);
+
+        nutrientIntakeChartFrame.setVisible(true);
+    }
+
+//    private void showNutrientIntakeChart() {
+//        // Create and display the NutrientIntakeChart
+//        JFrame nutrientIntakeChartFrame = new JFrame("Nutrient Intake Chart");
+//        nutrientIntakeChartFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//        nutrientIntakeChartFrame.setSize(1000, 800);
+//
+//        // Create an instance of NutrientIntakeChart
+//        NutrientIntakeChart nutrientIntakeChart = new NutrientIntakeChart("Nutrient Intake Chart");
+//
+//        // Add dummy data for testing (replace with actual data retrieval logic)
+//        nutrientIntakeChart.addDummyData();
+//
+//        // Add the NutrientIntakeChart to the NutrientIntakeChartFrame
+//        nutrientIntakeChartFrame.add(nutrientIntakeChart);
+//
+//        // Make the frame visible after setting the data
+//        nutrientIntakeChartFrame.setVisible(true);
+//
+//        // Call revalidate and repaint after the frame is visible
+//        nutrientIntakeChart.revalidate();
+//        nutrientIntakeChart.repaint();
+//    }
 
 
 
