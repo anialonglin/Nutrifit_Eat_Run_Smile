5bfe1655fe41e4cadce5f76052728cd8da759d21
Judah Goldstein <yehudah.lev@gmail.com>
2023-11-20 23:28:26 -0500

    get the code running again

--- Implimentation/Nutrifit/src/application/mealManager.java
+++ Implimentation/Nutrifit/src/application/mealManager.java
@@ -4,15 +4,11 @@ package application;
 import java.util.Date;
 import dataAccess.UserProfile.Database;
 public class mealManager {
-   public static void addProfile( String name,
-                                  int age,
-                                  String sex,
-                                  double height_cm,
-                                  double weight_kg){
+   public static void addProfile( String name, int age, String sex, double height_cm, double weight_kg){
        
        Database.getInstance().insertUserProfile(name,age,sex,height_cm,weight_kg);
    }
-
-    public static int addMeal(int profileId, String meal1, Date date) {
-    }
+//
+//    public static int addMeal(int profileId, String meal1, Date date) {
+//    }
 }
